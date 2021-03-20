b043a23fda58c08a79f18eb2fd7470b74ec3d377
Augustine <augustine.nani@gmail.com>
2021-03-20 07:11:14 +0000

    almost done with the crash(deleting members)

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@me.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@me.com',
+        status: 'inactive',
+    },
+    {
+        id: 3,
+        name: 'Derek Doe',
+        email: 'derek@me.com',
+        status: 'active',
+    },
+    {
+        id: 4,
+        name: 'Mavis Dean',
+        email: 'mavis@me.com',
+        status: 'inactive'
+    },
+]
+
+module.exports = members
